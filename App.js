3b748d4aae56fc38420db45f2314e20bf90b50da
Fawzy20 <fawzy-elsam@outlook.fr>
2021-11-22 13:28:18 +0100

    exo5

--- App.js
+++ App.js
@@ -1,66 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, View, Image, SafeAreaView, Text, Alert } from 'react-native';
-import { color } from 'react-native-elements/dist/helpers';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, ScrollView} from 'react-native';
 
 export default function App() {
     return ( 
-    <View style = { styles.container } >
-      <View style = { styles.header }>
-        <Text style = { styles.white }> Header </Text>
-      </View>
-      <View>
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'https://lesjoiesducode.fr/content/039/ea9v7hD.jpg',
-        }}
-      />
-      </View>
-      <View style = { styles.footer }>
-        <Text style={ styles.black}  >Footer</Text>
-      </View>
-    </View>
+      <ScrollView style = { styles.container }>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+        <View style = { styles.violet } />
+        <View style = { styles.blocRouge }>
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />
+          <View style = { styles.rouge } />     
+        </View>
+      </ScrollView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ecf0f1',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
+        backgroundColor: '#ffffff',
     },
-    logo: {
-      width: 400,
-      height: 500,
+    violet: {
+      width: 200,
+      height: 150,
+      backgroundColor: "#ff00fe",
     },
-    header: {
-      width: 400,
-      height: 100,
-      backgroundColor: '#000000',
-    },
-    footer: {
-      width: 400,
-      height: 100,
-      backgroundColor: '#ffffff',
-    },
-    black: {
-      fontSize: 20,
+    blocRouge: {
       display: 'flex',
-      alignItems: 'center',
-      color: 'black',
-      marginLeft: 160,
-      marginTop: 10,
+      flexDirection: 'row',
+      paddingTop: 10,
+      paddingBottom: 10
     },
-    white: {
-      fontSize: 20,
-      display: 'flex',
-      alignItems: 'center',
-      color: 'white',
-      marginLeft: 160,
-      marginTop: 50,
+    rouge: {
+      width: 100,
+      height: 120,
+      backgroundColor: "#fe0100",
+      marginRight: 10,
     }
 });
