6d92c81117586c7d454cf9d8657db151877615b7
Fawzy20 <fawzy-elsam@outlook.fr>
2021-11-22 21:46:25 +0100

    exo7

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text , TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
-    return ( <View style = { styles.container } >
-        <Text style={{ fontSize: 25 }}> Hello </Text> 
-        <StatusBar style = "auto" / >
-        </View>
+const FirstScreen = ({navigation}) => {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <TouchableOpacity onPress={() => navigation.navigate('Second')} >
+          <Text style={styles.text}>FirstPage</Text>
+        </TouchableOpacity>
+      </View>
     );
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#808080',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+  }
+  
+  
+  const SecondScreen = ({navigation}) => {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={() => navigation.navigate('First')} >
+              <Text style={styles.text}>SecondPage</Text>
+          </TouchableOpacity>
+        </View>
+      );
+  }
+  
+  const styles = StyleSheet.create({
+      text: {
+        color: "blue"
+      }
+    });
+  
+  const Stack = createNativeStackNavigator();
+  const Exo_07 = () => {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="First" component={FirstScreen} />
+          <Stack.Screen name="Second" component={SecondScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  export default Exo_07;
