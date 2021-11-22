ea2c95f106ff4870924740e854f00fda2e6e015b
Fawzy20 <fawzy-elsam@outlook.fr>
2021-11-22 12:59:38 +0100

    exo 3

--- App.js
+++ App.js
@@ -1,25 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Button, View, Image, SafeAreaView, Text, Alert } from 'react-native';
-import { color } from 'react-native-elements/dist/helpers';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
 export default function App() {
     return ( 
     <View style = { styles.container } >
-      <View style = { styles.header }>
-        <Text style = { styles.white }> Header </Text>
-      </View>
-      <View>
-      <Image
-        style={styles.logo}
-        source={{
-          uri: 'https://lesjoiesducode.fr/content/039/ea9v7hD.jpg',
-        }}
-      />
-      </View>
-      <View style = { styles.footer }>
-        <Text style={ styles.black}  >Footer</Text>
-      </View>
+      <Button title="BUTTON" onPress={() => Alert.alert('Simple Button pressed')}/>
     </View>
     );
 }
@@ -27,40 +13,11 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#ecf0f1',
+        backgroundColor: '#808080',
         alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-between',
+        justifyContent: 'center',
     },
-    logo: {
-      width: 400,
-      height: 500,
-    },
-    header: {
-      width: 400,
-      height: 100,
-      backgroundColor: '#000000',
-    },
-    footer: {
-      width: 400,
-      height: 100,
-      backgroundColor: '#ffffff',
-    },
-    black: {
-      fontSize: 20,
-      display: 'flex',
-      alignItems: 'center',
-      color: 'black',
-      marginLeft: 160,
-      marginTop: 10,
-    },
-    white: {
-      fontSize: 20,
-      display: 'flex',
-      alignItems: 'center',
-      color: 'white',
-      marginLeft: 160,
-      marginTop: 50,
-    }
+    Button: {
+        backgroundColor: '#2196f3',
+     }
 });
